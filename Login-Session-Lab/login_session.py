738a25263c7a4b9d1a9c437732a8d2a7a908f1e8
nouris24-meet <nour.stety@gmail.com>
2023-07-23 01:11:01 +0300

    It bothered me that I didn't do it so here ya go

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,22 +4,33 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+
+@app.route('/', methods=['GET', 'POST']) # What methods are needed?
 def home():
-	
+	if request.method == 'POST':
+		try: 
+			q = request.form['quote'] 
+			a = request.form['author'] 			
+			ag = request.form['age'] 
+			login_session['quote'] = str(q)
+			login_session['author'] = str(a)
+			login_session['age'] = int(ag)
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
+
+		
 	return render_template('home.html')
 
 
 @app.route('/error')
 def error():
-
 	return render_template('error.html')
 
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', quote=login_session['quote'], author=login_session['author'], age=login_session['age']) # What variables are needed?
 
 
 @app.route('/thanks')
